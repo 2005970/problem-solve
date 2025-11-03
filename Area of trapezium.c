a1b59b168e8184bb12a19270fb26eb5987ebd805
chandana <chandana4871@gmail.com>
2025-11-03 14:59:50 +0530

    Create Area of trapezium.c

---
+++ Area of trapezium.c
@@ -0,0 +1,18 @@
+Area of Trapezium
+Program Description
+Given the base1, base2, and height of Trapezium.Calculate the area of Trapezium.
+Note: Adjust the area upto four decimal points.
+
+#include<stdio.h>
+int main()
+{
+int b1,b2,h;
+scanf("%d",&b1);
+scanf("%d",&b2);
+scanf("%d",&h);
+
+double area=1/2*(b1+b2)*h;
+printf("%.4f",area);
+}
+return 0;
+}
